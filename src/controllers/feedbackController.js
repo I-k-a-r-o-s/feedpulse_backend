93e8f8309175e58d5ab3f2caf436356e05afc009
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-03-31 14:00:03 +0530

    added feedback controller wih add feedback function

---
+++ src/controllers/feedbackController.js
@@ -0,0 +1,34 @@
+import FeedbackModel from "../models/feedbackModel.js";
+
+export const addFeedback = async (req, res) => {
+  try {
+    const { title, description, category, submitterName, submitterEmail } =
+      req.body;
+    if (!title || !description || !category) {
+      return res.status(400).json({
+        success: false,
+        message: "Please provide all required fields!",
+      });
+    }
+
+    const feedback = await FeedbackModel.create({
+      title,
+      description,
+      category,
+      submitterName,
+      submitterEmail,
+    });
+    
+    return res.status(201).json({
+      success: true,
+      message: "Feedback created successfully!",
+      feedback,
+    });
+  } catch (error) {
+    console.error("Error in addFeedback!:", error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
